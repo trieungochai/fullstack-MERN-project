25e9898903ec00ad2f700aca4b9acfb0b127bea9
Macbook <trieungochai.dev@gmail.com>
2021-03-01 22:45:24 +0700

    Starting Setup, Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start</h1>
-}
+import NewPlace from './places/pages/NewPlace';
+import Users from './user/pages/Users';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/"/>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
