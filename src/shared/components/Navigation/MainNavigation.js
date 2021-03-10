fa9719e3ba22d34e420ad9c82b2e019b73e98264
Macbook <trieungochai.dev@gmail.com>
2021-03-10 11:08:30 +0700

    Implementing a Basic Desktop & Mobile Navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -4,22 +4,30 @@ import { Link } from 'react-router-dom';
 import './MainNavigation.css';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 
 const MainNavigation = props => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your place</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your place</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
