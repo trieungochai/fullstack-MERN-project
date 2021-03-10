16c8413acbd70e53eff82733ffd929279d28aea8
Macbook <trieungochai.dev@gmail.com>
2021-03-10 15:07:28 +0700

    Handling the Drawer State

--- src/shared/components/UIElements/Backdrop.js
+++ src/shared/components/UIElements/Backdrop.js
@@ -5,7 +5,7 @@ import './Backdrop.css';
 
 const Backdrop = props => {
   return ReactDOM.createPortal(
-    <div className="backdrop"></div>,
+    <div className="backdrop" onClick={props.onClick}></div>,
     document.getElementById('backdrop-hook')
   );
 };
