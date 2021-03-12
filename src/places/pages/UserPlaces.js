3488f1481a67f6c11c1f1478556fdfa36a85b16d
Macbook <trieungochai.dev@gmail.com>
2021-03-12 13:52:00 +0700

    Getting Route Params (react-router-dom useParams())

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
@@ -8,7 +9,7 @@ const DUMMY_PLACES = [
     title: 'Phong Nha - Ke Bang National Park Headquarter',
     description: 'Sprawling national park in lush surrounds featuring a network of wet caves, rivers & waterfalls.',
     imageUrl: 'https://vietnamdiscovery.com/wp-content/uploads/2019/11/Phong-nha-ke-bang-national-park.jpg',
-    address: 'Phong Nha, Bố Trạch District, Quảng Bình Province 511860, Vietnam',
+    address: 'Phong Nha, Bố Trạch, Quảng Bình 511860, Vietnam',
     location: {
       lat: 17.5907815,
       lng: 106.2812041
@@ -27,10 +28,24 @@ const DUMMY_PLACES = [
     },
     creator: 'u2'
   },
+  {
+    id: 'p3',
+    title: 'Thiên Mụ Pagoda',
+    description: 'Monks live in this famous 7-story Buddhist temple, built in 1601 & featuring landscaped grounds.',
+    imageUrl: 'https://i.imgur.com/Vpdg8Kq.jpg',
+    address: 'Hương Hòa, Thừa Thiên Huế 532761, Vietnam',
+    location: {
+      lat: 16.4532,
+      lng: 107.5426874
+    },
+    creator: 'u3'
+  },
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
